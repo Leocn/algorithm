945d16bcca8f2a1b8b9cb96431385535c7fdf074
Leocn <jlj888000@gmail.com>
2024-01-18 20:50:07 +0800

    几何题 线段和直线相交的个数

---
+++ codeforces/CF498/A.java
@@ -0,0 +1,183 @@
+package com.example.demo.codeforces.CF498;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.*;
+import java.util.InputMismatchException;
+
+public class A {
+    static RealFastReader sc = new RealFastReader(System.in);
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int t = 1;
+        while (t-- > 0) {
+            solve();
+        }
+        out.close();
+    }
+
+    public static void solve() {
+        long x1 = sc.nl();
+        long y1 = sc.nl();
+        long x2 = sc.nl();
+        long y2 = sc.nl();
+        int n = sc.ni();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            long a = sc.nl();
+            long b = sc.nl();
+            long c = sc.nl();
+
+            long r1 = a*x1 + b*y1 + c;
+            long r2 = a*x2 + b*y2 + c;
+            if(r1>0&& r2<0 || r1<0 && r2>0){
+                ans ++;
+            }
+        }
+        out.println(ans);
+    }
+
+    public static class RealFastReader {
+        InputStream is;
+
+        public RealFastReader(final InputStream is) {
+            this.is = is;
+        }
+
+        private byte[] inbuf = new byte[8192];
+        public int lenbuf = 0, ptrbuf = 0;
+
+        public int readByte() {
+            if (lenbuf == -1) {
+                throw new InputMismatchException();
+            }
+            if (ptrbuf >= lenbuf) {
+                ptrbuf = 0;
+                try {
+                    lenbuf = is.read(inbuf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (lenbuf <= 0) {
+                    return -1;
+                }
+            }
+            return inbuf[ptrbuf++];
+        }
+
+        private boolean isSpaceChar(int c) {
+            return !(c >= 33 && c <= 126);
+        }
+
+        private int skip() {
+            int b;
+            while ((b = readByte()) != -1 && isSpaceChar(b))
+                ;
+            return b;
+        }
+
+        public double nd() {
+            return Double.parseDouble(ns());
+        }
+
+        public char nc() {
+            return (char) skip();
+        }
+
+        public String ns() {
+            int b = skip();
+            StringBuilder sb = new StringBuilder();
+            while (!(isSpaceChar(b))) { // when nextLine, (isSpaceChar(b) && b != ' ')
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
+
+        public char[] ns(int n) {
+            char[] buf = new char[n];
+            int b = skip(), p = 0;
+            while (p < n && !(isSpaceChar(b))) {
+                buf[p++] = (char) b;
+                b = readByte();
+            }
+            return n == p ? buf : Arrays.copyOf(buf, p);
+        }
+
+        public int[] na(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = ni();
+            }
+            return a;
+        }
+
+        public long[] nal(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nl();
+            }
+            return a;
+        }
+
+        public char[][] nm(int n, int m) {
+            char[][] map = new char[n][];
+            for (int i = 0; i < n; i++) {
+                map[i] = ns(m);
+            }
+            return map;
+        }
+
+        public int[][] nmi(int n, int m) {
+            int[][] map = new int[n][];
+            for (int i = 0; i < n; i++) {
+                map[i] = na(m);
+            }
+            return map;
+        }
+
+        public int ni() {
+            int num = 0;
+            int b;
+            boolean minus = false;
+            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+                ;
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+
+            while (true) {
+                if (b >= '0' && b <= '9') {
+                    num = num * 10 + (b - '0');
+                } else {
+                    return minus ? -num : num;
+                }
+                b = readByte();
+            }
+        }
+
+        public long nl() {
+            long num = 0;
+            int b;
+            boolean minus = false;
+            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+                ;
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+
+            while (true) {
+                if (b >= '0' && b <= '9') {
+                    num = num * 10 + (b - '0');
+                } else {
+                    return minus ? -num : num;
+                }
+                b = readByte();
+            }
+        }
+    }
+}
