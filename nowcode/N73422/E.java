15dc6813ee923adec1d69b5292f808ba49bbfe9d
Leocn <jlj888000@gmail.com>
2024-01-21 21:58:18 +0800

    牛客周赛29

---
+++ nowcode/N73422/E.java
@@ -0,0 +1,268 @@
+package com.example.demo.nowcode.N73422;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.*;
+import java.util.InputMismatchException;
+
+public class E {
+    static RealFastReader sc = new RealFastReader(System.in);
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int t = 1;
+        while (t-- > 0) {
+            solve();
+        }
+        out.close();
+    }
+
+    public static void solve() {
+        long n = sc.nl();
+        if(n==1){
+            out.println(-1);
+            return;
+        }
+        List<Long> f = getPrimeFactors(n);
+        List<long[]> list = new ArrayList<>();
+        for(long d: f){
+            int c = 0;
+            while (n%d==0){
+                n/=d;
+                c++;
+            }
+            list.add(new long[]{d,c});
+        }
+
+        Collections.sort(list, (a,b)->Long.compare(b[1],a[1]));
+        List<Long> l = new ArrayList<>();
+        for(long[] d: list){
+            long t = d[1];
+            while (t>0){
+                l.add(d[0]);
+                t--;
+            }
+        }
+
+
+
+        int loc = 0;
+        int m = l.size();
+        long[] ans = new long[m];
+        for (int i = 0; i < m; i+=2) {
+            ans[i] = l.get(loc++);
+        }
+        for (int i = 1; i < m; i+=2) {
+            ans[i] = l.get(loc++);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            if(i>0&& ans[i] == ans[i-1]){
+                out.println(-1);
+                return;
+            }
+            sb.append(ans[i]).append(" ");
+        }
+        out.println(m);
+        out.println(sb);
+    }
+    static final List<Integer> PRIMES = getPrimesLe(10000000);
+
+    public static List<Integer> getPrimesLe(int n) {
+        boolean[] arr = new boolean[n+1];
+        Arrays.fill(arr, true);
+        arr[1] = false;
+        int p = 2;
+        int r = (int) (1 + Math.sqrt(n));
+        while (p <= r) {
+            int v = p + p;
+            while (v <= n) {
+                arr[v] = false;
+                v += p;
+            }
+            p++;
+            while (p <= r && !arr[p]) {
+                p++;
+            }
+        }
+
+        List<Integer> primes = new ArrayList<>();
+        for (int i = 2; i <= n; i++) {
+            if (arr[i]) {
+                primes.add(i);
+            }
+        }
+        //show(primes);
+        return primes;
+    }
+
+    /**
+     ** 获取质数因子
+     * */
+    public static List<Long> getPrimeFactors(long k) {
+        List<Long> ans = new ArrayList<>();
+        Long r = (long) Math.sqrt(k) + 1;
+        for (long p : PRIMES) {
+            if (p > r) {
+                break;
+            }
+            long exp = 0;
+            while (k % p == 0) {
+                exp++;
+                k /= p;
+            }
+            if (exp > 0) {
+                ans.add(p);
+            }
+        }
+        if (k != 1) {
+            ans.add(k);
+        }
+        return ans;
+    }
+
+
+    public static class RealFastReader {
+        InputStream is;
+
+        public RealFastReader(final InputStream is) {
+            this.is = is;
+        }
+
+        private byte[] inbuf = new byte[8192];
+        public int lenbuf = 0, ptrbuf = 0;
+
+        public int readByte() {
+            if (lenbuf == -1) {
+                throw new InputMismatchException();
+            }
+            if (ptrbuf >= lenbuf) {
+                ptrbuf = 0;
+                try {
+                    lenbuf = is.read(inbuf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (lenbuf <= 0) {
+                    return -1;
+                }
+            }
+            return inbuf[ptrbuf++];
+        }
+
+        private boolean isSpaceChar(int c) {
+            return !(c >= 33 && c <= 126);
+        }
+
+        private int skip() {
+            int b;
+            while ((b = readByte()) != -1 && isSpaceChar(b))
+                ;
+            return b;
+        }
+
+        public double nd() {
+            return Double.parseDouble(ns());
+        }
+
+        public char nc() {
+            return (char) skip();
+        }
+
+        public String ns() {
+            int b = skip();
+            StringBuilder sb = new StringBuilder();
+            while (!(isSpaceChar(b))) { // when nextLine, (isSpaceChar(b) && b != ' ')
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
+
+        public char[] ns(int n) {
+            char[] buf = new char[n];
+            int b = skip(), p = 0;
+            while (p < n && !(isSpaceChar(b))) {
+                buf[p++] = (char) b;
+                b = readByte();
+            }
+            return n == p ? buf : Arrays.copyOf(buf, p);
+        }
+
+        public int[] na(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = ni();
+            }
+            return a;
+        }
+
+        public long[] nal(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nl();
+            }
+            return a;
+        }
+
+        public char[][] nm(int n, int m) {
+            char[][] map = new char[n][];
+            for (int i = 0; i < n; i++) {
+                map[i] = ns(m);
+            }
+            return map;
+        }
+
+        public int[][] nmi(int n, int m) {
+            int[][] map = new int[n][];
+            for (int i = 0; i < n; i++) {
+                map[i] = na(m);
+            }
+            return map;
+        }
+
+        public int ni() {
+            int num = 0;
+            int b;
+            boolean minus = false;
+            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+                ;
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+
+            while (true) {
+                if (b >= '0' && b <= '9') {
+                    num = num * 10 + (b - '0');
+                } else {
+                    return minus ? -num : num;
+                }
+                b = readByte();
+            }
+        }
+
+        public long nl() {
+            long num = 0;
+            int b;
+            boolean minus = false;
+            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+                ;
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+
+            while (true) {
+                if (b >= '0' && b <= '9') {
+                    num = num * 10 + (b - '0');
+                } else {
+                    return minus ? -num : num;
+                }
+                b = readByte();
+            }
+        }
+    }
+}
