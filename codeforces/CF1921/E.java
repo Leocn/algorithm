63ef43fd41c54768535dd172d41f68897b25b8b5
Leocn <jlj888000@gmail.com>
2024-01-25 23:14:37 +0800

    数学题

---
+++ codeforces/CF1921/E.java
@@ -0,0 +1,211 @@
+package com.example.demo.codeforces.CF1921;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.*;
+import java.util.InputMismatchException;
+
+public class E {
+    static RealFastReader sc = new RealFastReader(System.in);
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int t = sc.ni();
+        while (t-- > 0) {
+            solve();
+        }
+        out.close();
+    }
+
+    public static void solve() {
+        int h = sc.ni();
+        int w = sc.ni();
+        int x1 = sc.ni();
+        int y1 = sc.ni();
+        int x2 = sc.ni();
+        int y2 = sc.ni();
+        if(x1>=x2){
+            out.println("Draw");
+            return;
+        }
+        int sum =  Math.abs(x1-x2);
+        if((sum&1)==1){
+            if(y1>y2+1){
+                if(sum - 2*(y2-1)+1<2*(y1-y2)){
+                    out.println("Draw");
+                    return;
+                }
+
+            }else if(y2>y1+1){
+                if(sum - 2*(w-y2)+1<2*(y2-y1)){
+                    out.println("Draw");
+                    return;
+                }
+            }
+
+            out.println("Alice");
+        }else {
+            if(y1>y2){
+                if(sum - 2*(w-y1)+1<2*(y1-y2)){
+                    out.println("Draw");
+                    return;
+                }
+            }else if(y2> y1){
+                if(sum - 2*(y1-1)+1<2*(y2-y1)){
+                    out.println("Draw");
+                    return;
+                }
+            }
+
+            out.println("Bob");
+        }
+
+
+
+
+
+    }
+
+    public static class RealFastReader {
+        InputStream is;
+
+        public RealFastReader(final InputStream is) {
+            this.is = is;
+        }
+
+        private byte[] inbuf = new byte[8192];
+        public int lenbuf = 0, ptrbuf = 0;
+
+        public int readByte() {
+            if (lenbuf == -1) {
+                throw new InputMismatchException();
+            }
+            if (ptrbuf >= lenbuf) {
+                ptrbuf = 0;
+                try {
+                    lenbuf = is.read(inbuf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (lenbuf <= 0) {
+                    return -1;
+                }
+            }
+            return inbuf[ptrbuf++];
+        }
+
+        private boolean isSpaceChar(int c) {
+            return !(c >= 33 && c <= 126);
+        }
+
+        private int skip() {
+            int b;
+            while ((b = readByte()) != -1 && isSpaceChar(b))
+                ;
+            return b;
+        }
+
+        public double nd() {
+            return Double.parseDouble(ns());
+        }
+
+        public char nc() {
+            return (char) skip();
+        }
+
+        public String ns() {
+            int b = skip();
+            StringBuilder sb = new StringBuilder();
+            while (!(isSpaceChar(b))) { // when nextLine, (isSpaceChar(b) && b != ' ')
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
+
+        public char[] ns(int n) {
+            char[] buf = new char[n];
+            int b = skip(), p = 0;
+            while (p < n && !(isSpaceChar(b))) {
+                buf[p++] = (char) b;
+                b = readByte();
+            }
+            return n == p ? buf : Arrays.copyOf(buf, p);
+        }
+
+        public int[] na(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = ni();
+            }
+            return a;
+        }
+
+        public long[] nal(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nl();
+            }
+            return a;
+        }
+
+        public char[][] nm(int n, int m) {
+            char[][] map = new char[n][];
+            for (int i = 0; i < n; i++) {
+                map[i] = ns(m);
+            }
+            return map;
+        }
+
+        public int[][] nmi(int n, int m) {
+            int[][] map = new int[n][];
+            for (int i = 0; i < n; i++) {
+                map[i] = na(m);
+            }
+            return map;
+        }
+
+        public int ni() {
+            int num = 0;
+            int b;
+            boolean minus = false;
+            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+                ;
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+
+            while (true) {
+                if (b >= '0' && b <= '9') {
+                    num = num * 10 + (b - '0');
+                } else {
+                    return minus ? -num : num;
+                }
+                b = readByte();
+            }
+        }
+
+        public long nl() {
+            long num = 0;
+            int b;
+            boolean minus = false;
+            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+                ;
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+
+            while (true) {
+                if (b >= '0' && b <= '9') {
+                    num = num * 10 + (b - '0');
+                } else {
+                    return minus ? -num : num;
+                }
+                b = readByte();
+            }
+        }
+    }
+}
